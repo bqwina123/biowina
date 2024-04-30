892907cfea8de4191bab89fa3329183f72634b3b
bqwina123 <164603642+bqwina123@users.noreply.github.com>
2024-04-30 18:25:24 +0800

    Update latihan.py
    BaiqWinaNurhiladia

--- latihan.py
+++ latihan.py
@@ -1,27 +1,28 @@
+# daftar barang beserta harganya
 daftar_barang = {
-    "Beras": 10000,
-    "Gula" : 8000,
-    "Telur": 2000,
-    "Minyak goreng": 15000,
-    "Garam" : 5000,
+    "beras": 10000,
+    "gula": 8000,
+    "telur": 2000,
+    "minyak goreng": 15000,
+    "garam": 5000
 }
 
-# Menampilkan daftar barang
-print("Daftar Barang:")
+# menampilkan daftar barang
+print("daftar barang:")
 for barang, harga in daftar_barang.items():
-    print(f"{barang}: Rp {harga}")
+    print(f"{barang}: rp {harga}")
 
 # input jumlah barang yang dibeli
 total_belanja = 0
 jumlah_barang = int(input("\nMasukan jumlah barang yang dibeli: "))
 
-# Menghitung total belanjaan
+# menghitung total belanjaan
 for i in range(jumlah_barang):
-    barang = input(f"Masukan nama barang barang ke-{i+1}: ")
+    barang = input (f"masukan nama barang ke-{i+1}: ")
     if barang in daftar_barang:
         total_belanja += daftar_barang[barang]
     else:
-            print(f"{barang} tidak ada dalam daftar barang.")
+        print(f"{barang} tidak ada dalam daftar barang.")
 
-# Menampilkan total belanjaan
-print(f"\Total belanjaan anda: Rp {total_belanja}")
+# menampilkan total belanjaan
+print(f"\nTotal belanjaan anda: rp {total_belanja}")
